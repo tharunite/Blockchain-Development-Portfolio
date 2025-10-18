8c4631333f41e4742901a60675fa892e0c684923
UTK <utharunkumar0@gmail.com>
2025-10-19 01:06:22 +0530

    Reorganize project structure

--- current_price.py
+++
@@ -1,5 +0,0 @@
-import requests
-
-data=requests.get("https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd")
-price=data.json()
-print(f"Bitcoin: ${price['bitcoin']['usd']:,.2f}")
