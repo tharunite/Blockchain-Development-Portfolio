ff94dd081d303ecd555d7e111022d8cdc5fc19a9
UTK <utharunkumar0@gmail.com>
2025-10-20 03:24:42 +0530

    Changes to file structure

--- cryptography basics/tamper_check/untampered_sender.py
+++
@@ -1,6 +0,0 @@
-from hashlib import sha256
-message='This is to check if the integrity of a message sent via public network'
-data=sha256((message.strip() + 'p@ssword').encode()).hexdigest()
-with open('message','w') as f:
-    f.write(message)
-    f.write(f'\n{data}')
